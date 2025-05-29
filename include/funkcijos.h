ce5cfd6c6c1e04bbed23f8c50069dba8910acc98
Nika <bukolovaite.nika@gmail.com>
2025-05-29 21:23:55 +0300

    Tikrai veikia viskas

--- include/funkcijos.h
+++ include/funkcijos.h
@@ -4,12 +4,12 @@
 
 const string aplankalo_pavadinimas = "NuskaitymoFailai";
 string koki_faila_nuskaityti();
-string skyrybos_zenklai(const string &zodis);
+wstring skyrybos_zenklai(const string &zodis);
 void suskaiciuoti_kiek_zodziu_ir_irasyti_rezultata(const string &failo_pavadinimas);
 void cross_reference_lentele(const string &failo_pavadinimas);
-string gauti_url_link(const string &zodis);
-string pasalinti_galinius_skyrybos_zenklus(const string &zodis);
-bool ar_zodis_yra_url(const string &zodis, const set<string> &url_linkai);
+wstring gauti_url_link(const wstring &zodis);
+wstring pasalinti_galinius_skyrybos_zenklus(const wstring &zodis);
+bool ar_zodis_yra_url(const wstring &zodis, const set<wstring> &url_linkai);
 void rasti_visus_galimus_url(const string &teksto_failas, const string &url_sarasas);
 
 #endif
--- src/funkcijos.cpp
+++ src/funkcijos.cpp
@@ -37,16 +37,17 @@ string koki_faila_nuskaityti()
 	}
 }
 
-string skyrybos_zenklai(const string &zodis)
+wstring skyrybos_zenklai(const wstring &zodis)
 {
-	string rezultatas{};
-	string papildomi_skyrybos = "–„′−";
+	wstring rezultatas{};
+	wstring papildomi_skyrybos = L"–„′−";
 
-	for (const unsigned char c : zodis)
+	for (wchar_t c : zodis)
 	{
-		if (!std::ispunct(c) && !std::isdigit(c) && papildomi_skyrybos.find(c) == string::npos)
+
+		if (!std::iswpunct(c) && !std::iswdigit(c) && papildomi_skyrybos.find(c) == string::npos)
 		{
-			rezultatas += tolower(c);
+			rezultatas += towlower(c);
 		}
 	}
 	return rezultatas;
@@ -54,27 +55,27 @@ string skyrybos_zenklai(const string &zodis)
 
 void suskaiciuoti_kiek_zodziu_ir_irasyti_rezultata(const string &failo_pavadinimas)
 {
-	ifstream failas(failo_pavadinimas);
+	wifstream failas(failo_pavadinimas);
 	if (!failas)
 	{
 		throw std::ios_base::failure("Klaida: nepavyko atidaryti failo!");
 		return;
 	}
-	stringstream buferis;
+	wstringstream buferis;
 	buferis << failas.rdbuf();
 	failas.close();
 
-	map<string, int> zodziu_kiekis;
-	string zodis;
+	map<wstring, int> zodziu_kiekis;
+	wstring zodis;
 	while (buferis >> zodis)
 	{
-		string svarus_zodis = skyrybos_zenklai(zodis);
+		wstring svarus_zodis = skyrybos_zenklai(zodis);
 		if (!svarus_zodis.empty())
 		{
 			zodziu_kiekis[svarus_zodis]++;
 		}
 	}
-	ofstream rezultatai("OutputFailai/zodziu_dazniai.txt");
+	wofstream rezultatai("OutputFailai/zodziu_dazniai.txt");
 	for (const auto &pora : zodziu_kiekis)
 	{
 		if (pora.second > 1)
@@ -88,29 +89,29 @@ void suskaiciuoti_kiek_zodziu_ir_irasyti_rezultata(const string &failo_pavadinim
 
 void cross_reference_lentele(const string &failo_pavadinimas)
 {
-	ifstream failas(failo_pavadinimas);
+	wifstream failas(failo_pavadinimas);
 	if (!failas)
 	{
 		throw std::ios_base::failure("Klaida: nepavyko atidaryti failo!");
 		return;
 	}
-	stringstream buferis;
+	wstringstream buferis;
 	buferis << failas.rdbuf();
 	failas.close();
 
-	map<string, int> zodziu_kiekis;
-	map<string, set<int>> zodziu_eilutes;
+	map<wstring, int> zodziu_kiekis;
+	map<wstring, set<int>> zodziu_eilutes;
 
-	string eilute;
+	wstring eilute;
 	int eilutes_nr = 1;
 
 	while (getline(buferis, eilute))
 	{
-		stringstream ss(eilute);
-		string zodis;
+		wstringstream ss(eilute);
+		wstring zodis;
 		while (ss >> zodis)
 		{
-			string svarus = skyrybos_zenklai(zodis);
+			wstring svarus = skyrybos_zenklai(zodis);
 			if (!svarus.empty())
 			{
 				zodziu_kiekis[svarus]++;
@@ -119,7 +120,7 @@ void cross_reference_lentele(const string &failo_pavadinimas)
 		}
 		eilutes_nr++;
 	}
-	ofstream out("OutputFailai/cross_reference.txt");
+	wofstream out("OutputFailai/cross_reference.txt");
 	if (!out)
 	{
 		throw std::ios_base::failure("Klaida: nepavyko atidaryti failo!");
@@ -128,12 +129,12 @@ void cross_reference_lentele(const string &failo_pavadinimas)
 	else
 	{
 		out << left << setw(30) << "Zodis" << "Eilutes\n";
-		out << string(30, '-') << " " << string(40, '-') << "\n";
+		out << wstring(30, '-') << " " << wstring(40, '-') << "\n";
 		for (const auto &[zodis, eilutes] : zodziu_eilutes)
 		{
 			if (zodziu_kiekis[zodis] > 1)
 			{
-				stringstream eilutes_srautui;
+				wstringstream eilutes_srautui;
 				bool pirma = true;
 				for (int e : eilutes)
 				{
@@ -149,14 +150,14 @@ void cross_reference_lentele(const string &failo_pavadinimas)
 	cout << "Cross-reference lentelė įrašyta į 'OutputFailai/cross_reference.txt'\n";
 }
 
-string gauti_url_link(const string &zodis)
+wstring gauti_url_link(const wstring &zodis)
 {
 	size_t paskutinis_taskas = zodis.rfind('.');
-	if (paskutinis_taskas == string::npos || paskutinis_taskas + 1 >= zodis.size())
+	if (paskutinis_taskas == wstring::npos || paskutinis_taskas + 1 >= zodis.size())
 	{
-		return "";
+		return L"";
 	}
-	string galune = zodis.substr(paskutinis_taskas + 1);
+	wstring galune = zodis.substr(paskutinis_taskas + 1);
 	size_t galas = 0;
 	while (galas < galune.size() && isalpha(galune[galas]))
 	{
@@ -165,7 +166,7 @@ string gauti_url_link(const string &zodis)
 	return galune.substr(0, galas);
 }
 
-string pasalinti_galinius_skyrybos_zenklus(const string &zodis)
+wstring pasalinti_galinius_skyrybos_zenklus(const wstring &zodis)
 {
 	size_t pabaiga = zodis.size();
 	while (pabaiga > 0 && ispunct(zodis[pabaiga - 1]) && zodis[pabaiga - 1] != '/')
@@ -175,15 +176,15 @@ string pasalinti_galinius_skyrybos_zenklus(const string &zodis)
 	return zodis.substr(0, pabaiga);
 }
 
-bool ar_zodis_yra_url(const string &zodis, const set<string> &url_linkai)
+bool ar_zodis_yra_url(const wstring &zodis, const set<wstring> &url_linkai)
 {
 	size_t tasko_pozicija = zodis.find('.');
-	if (tasko_pozicija == string::npos || tasko_pozicija + 1 >= zodis.size())
+	if (tasko_pozicija == wstring::npos || tasko_pozicija + 1 >= zodis.size())
 	{
 		return false;
 	}
 
-	string galune = zodis.substr(tasko_pozicija + 1);
+	wstring galune = zodis.substr(tasko_pozicija + 1);
 	size_t end = 0;
 	while (end < galune.size() && isalpha(galune[end]))
 		end++;
@@ -194,11 +195,11 @@ bool ar_zodis_yra_url(const string &zodis, const set<string> &url_linkai)
 	{
 		return false;
 	}
-	if (zodis.find("http://") == 0 || zodis.find("https://") == 0 || zodis.find("www.") == 0)
+	if (zodis.find(L"http://") == 0 || zodis.find(L"https://") == 0 || zodis.find(L"www.") == 0)
 	{
 		return true;
 	}
-	if (isalnum(zodis[0]) && zodis.find(' ') == string::npos)
+	if (isalnum(zodis[0]) && zodis.find(' ') == wstring::npos)
 	{
 		return true;
 	}
@@ -208,14 +209,14 @@ bool ar_zodis_yra_url(const string &zodis, const set<string> &url_linkai)
 void rasti_visus_galimus_url(const string &teksto_failas, const string &url_sarasas)
 {
 	// Ikeliu URL sarasa
-	ifstream url_failas(url_sarasas);
+	wifstream url_failas(url_sarasas);
 	if (!url_failas)
 	{
 		throw std::ios_base::failure("Klaida: nepavyko atidaryti URL failo!");
 		return;
 	}
-	set<string> url_linkai;
-	string eilute;
+	set<wstring> url_linkai;
+	wstring eilute;
 	while (getline(url_failas, eilute))
 	{
 		if (!eilute.empty())
@@ -227,23 +228,23 @@ void rasti_visus_galimus_url(const string &teksto_failas, const string &url_sara
 	url_failas.close();
 
 	// Ikeliu nagrinejama teksta
-	ifstream tekstas(teksto_failas);
+	wifstream tekstas(teksto_failas);
 	if (!tekstas)
 	{
 		throw std::ios_base::failure("Klaida: nepavyko atidaryti teksto failo!");
 		return;
 	}
 
-	stringstream buferis;
+	wstringstream buferis;
 	buferis << tekstas.rdbuf();
 	tekstas.close();
 
 	// Tikrinu kiekviena zodi
-	set<string> rasti_url;
-	string zodis;
+	set<wstring> rasti_url;
+	wstring zodis;
 	while (buferis >> zodis)
 	{
-		string isvalytas = pasalinti_galinius_skyrybos_zenklus(zodis);
+		wstring isvalytas = pasalinti_galinius_skyrybos_zenklus(zodis);
 		if (ar_zodis_yra_url(isvalytas, url_linkai))
 		{
 			rasti_url.insert(isvalytas);
@@ -251,18 +252,18 @@ void rasti_visus_galimus_url(const string &teksto_failas, const string &url_sara
 	}
 
 	// Irasome rezultatus i faila
-	ofstream rezultatai("OutputFailai/rastieji_url.txt");
+	wofstream rezultatai("OutputFailai/rastieji_url.txt");
 	if (!rezultatai)
 	{
 		throw std::ios_base::failure("Klaida: nepavyko atidaryti rezultatu failo!");
 		return;
 	}
 	rezultatai << "Rasti URL'ai:\n"
-			   << string(40, '-') << "\n";
+			   << wstring(40, '-') << "\n";
 	for (const auto &url : rasti_url)
 	{
 		rezultatai << url << "\n";
 	}
 
-	cout << "Rasti URL'ai įrašyti į 'OutputFailai/rastieji_url.txt'\n";
+	wcout << "Rasti URL'ai įrašyti į 'OutputFailai/rastieji_url.txt'\n";
 }
